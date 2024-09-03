5020592349bd51b7733b11487721e0865d19533b
Bettina Bernarda da Silva <156723691+BettBernarda@users.noreply.github.com>
2024-09-03 17:44:37 -0300

    Learning about variables  archives

---
+++ learning variables/Types.java
@@ -0,0 +1,14 @@
+ public class Types{
+ public static void main(String [] arg){
+   
+    //We need to declare the type of the variable before its name
+    String name = "James Gosling";
+    int yearCreated = 1995;
+    System.out.println(name);
+    System.out.println(yearCreated);
+    
+    
+ }
+}
+
+ //fun fact The int data type allows values between -2,147,483,648 and 2,147,483,647, inclusive
