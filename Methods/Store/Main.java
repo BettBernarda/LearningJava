8d935025a53dceddaa85581a7e063d26896ae940
Bettina Bernarda da Silva <156723691+BettBernarda@users.noreply.github.com>
2024-09-10 12:45:46 -0300

    forgot ;

--- Methods/Store/Main.java
+++ Methods/Store/Main.java
@@ -1,6 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-    Store lemonadeStand = new Store("Lemonade")
+    Store lemonadeStand = new Store("Lemonade");
     lemonadeStand.advertise();
   }
 }
