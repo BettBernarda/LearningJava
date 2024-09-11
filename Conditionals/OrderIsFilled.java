f85944e965a924613bd044427cba9f9f88a9435e
Bettina Bernarda da Silva <156723691+BettBernarda@users.noreply.github.com>
2024-09-11 12:34:59 -0300

    Create OrderIsFilled.java

---
+++ Conditionals/OrderIsFilled.java
@@ -0,0 +1,18 @@
+public class OrderIsFilled {
+  
+  public static void main(String[] args) {
+    
+    boolean isFilled = false;
+    
+    // Write an if-then-else statement:
+    if (isFilled){
+      System.out.println("Shipping");
+
+    }else{
+      System.out.println("Order not ready!");
+    }
+    
+    
+  
+  }
+}
